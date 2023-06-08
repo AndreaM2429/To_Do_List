e20bbc5ace5818f400d3be65cc98426484eeedf2
AndreaMOr <andreamanuel2207@gmail.com>
2023-06-07 21:49:39 -0600

    Refractor code with same funtionalities

--- src/index.js
+++ src/index.js
@@ -1,19 +1,4 @@
 import './style.css';
-import List from './list.js';
+import Taks from './Modules/main_class.js';
 
-const todoList = new List();
-todoList.display();
-document.querySelector('#add-task').addEventListener('submit', (e) => {
-  e.preventDefault();
-  const activity = e.target.elements.activity.value;
-  todoList.addActivity(activity);
-  e.target.reset();
-});
-
-document.querySelector('.clear-completed').addEventListener('click', () => {
-  todoList.clearCompleted();
-});
-
-document.querySelector('#delete-all').addEventListener('click', () => {
-  todoList.clearAll();
-});
+Taks.init();
